21f986d9379b19f5c0a5a2964ab4dc5c1f89b1b7
aenulrofik <71721661+aenulrofik@users.noreply.github.com>
2023-04-09 02:23:17 +0800

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -111,30 +111,28 @@ def get_readable_message():
         globals()['PAGE_NO'] -= 1
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
         if download.message.chat.type.name in ['SUPERGROUP', 'CHANNEL']:
-            msg += f"<b>_____《🐱 Pik4Bot 🐱》_____</b>"
-            msg += f"\n\n<b>☞ <a href='{download.message.link}'>{download.status()}</a>: </b>"
+            msg += f"<b><a href='{download.message.link}'>{download.status()}</a>: </b>"
         else:
-            msg += f"<b>_____《🐱 Pik4Bot 🐱》_____</b>"
-            msg += f"\n\n<b>☞ {download.status()}: </b>"
+            msg += f"<b>{download.status()}: </b>"
         msg += f"<code>{escape(f'{download.name()}')}</code>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
-            msg += f"\n<b>☞</b> {get_progress_bar_string(download.progress())} {download.progress()}"
-            msg += f"\n<b>☞ Processed:</b> {download.processed_bytes()} of {download.size()}"
-            msg += f"\n<b>☞ Speed:</b> {download.speed()} | <b>ETA:</b> {download.eta()}"
+            msg += f"\n{get_progress_bar_string(download.progress())} {download.progress()}"
+            msg += f"\n<b>Processed:</b> {download.processed_bytes()} of {download.size()}"
+            msg += f"\n<b>Speed:</b> {download.speed()} | <b>ETA:</b> {download.eta()}"
             if hasattr(download, 'seeders_num'):
                 try:
-                    msg += f"\n<b>☞ Seeders:</b> {download.seeders_num()} | <b>Leechers:</b> {download.leechers_num()}"
-               except:
+                    msg += f"\n<b>Seeders:</b> {download.seeders_num()} | <b>Leechers:</b> {download.leechers_num()}"
+                except:
                     pass
         elif download.status() == MirrorStatus.STATUS_SEEDING:
-            msg += f"\n<b>☞ Size: </b>{download.size()}"
-            msg += f"\n<b>☞ Speed: </b>{download.upload_speed()}"
-            msg += f" | <b>☞ Uploaded: </b>{download.uploaded_bytes()}"
+            msg += f"\n<b>Size: </b>{download.size()}"
+            msg += f"\n<b>Speed: </b>{download.upload_speed()}"
+            msg += f" | <b>Uploaded: </b>{download.uploaded_bytes()}"
             msg += f"\n<b>Ratio: </b>{download.ratio()}"
-            msg += f" | <b>☞ Time: </b>{download.seeding_time()}"
+            msg += f" | <b>Time: </b>{download.seeding_time()}"
         else:
-            msg += f"\n<b>☞ Size: </b>{download.size()}"
-        msg += f"\n<b>To Cancel:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>\n\n"
+            msg += f"\n<b>Size: </b>{download.size()}"
+        msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>\n\n"
     if len(msg) == 0:
         return None, None
     dl_speed = 0
@@ -166,9 +164,9 @@ def get_readable_message():
         buttons.ibutton(">>", "status nex")
         buttons.ibutton("♻️", "status ref")
         button = buttons.build_menu(3)
-    msg += f"┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+    msg += f"<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}"
     msg += f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {get_readable_time(time() - botStartTime)}"
-    msg += f"\n<b>🔻:</b> {get_readable_file_size(dl_speed)}/s | <b>🔺:</b> {get_readable_file_size(up_speed)}/s"
+    msg += f"\n<b>DL:</b> {get_readable_file_size(dl_speed)}/s | <b>UL:</b> {get_readable_file_size(up_speed)}/s"
     return msg, button
 
 
